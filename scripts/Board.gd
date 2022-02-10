a7f0442bd8cadcfd9bbc419b53b7f4644a6dd409
Rylan Schubkegel <rylanschubkegel@gmail.com>
2022-02-09 17:14:34 -0800

    Added automatic turn skip when no valid moves are available

--- scripts/Board.gd
+++ scripts/Board.gd
@@ -4,7 +4,7 @@ const Token = preload("res://scripts/Token.gd")
 const PAWN_IMG = preload("res://images/chess_pawn.png")
 const DICE_IMGS = [preload("res://images/dice_0.png"), preload("res://images/dice_1.png")]
 const PLAYER_COLORS = [Color(0.10, 0.25, 0.45), Color(0.45, 0.16, 0.15)]
-const PLAYER_TOKEN_COUNT = 7
+const PLAYER_TOKEN_COUNT = 1
 
 var cur_roll = 0
 var cur_player = 1
@@ -16,7 +16,7 @@ var dice_nodes = []
 
 
 # connects all buttons to player tile list
-func load_tiles():
+func load_player_tiles():
 	player_tiles[0].append($rows/tiles/cols5/btn1)
 	player_tiles[0].append($rows/tiles/cols4/btn1)
 	player_tiles[0].append($rows/tiles/cols3/btn1)
@@ -63,9 +63,8 @@ func assign_dice():
 func _ready():
 	
 	# init variables
-	load_tiles()
+	load_player_tiles()
 	assign_dice()
-	
 	reroll_tiles = get_tree().get_nodes_in_group("reroll_tiles")
 	
 	# connect button press to click function
@@ -84,47 +83,89 @@ func _ready():
 	$rows/tiles/cols6/p2counter.modulate = PLAYER_COLORS[1]
 	update_counters()
 	
-	# display the first player
-	print("Player ", str(cur_player), "'s turn")
+	# make the first move
 	randomize()
 	roll_dice()
+	update_player_message()
 
 
 # called by button when pressed
 func tile_clicked(tile):
-	if cur_roll == 0:
-		print("You rolled a 0!")
-		return
-	
-	# did the player play on their own path?
-	if tile in player_tiles[cur_player - 1]:
+	# only do something with a tile in the current player's path
+	# when that player's roll is greater than 0
+	if cur_roll > 0 and tile in player_tiles[cur_player - 1]:
 		
 		# token is currently selected; player is trying to place tile
 		if selected_token:
-			place_selected_token(tile)
+			
+			# allow deselection of tokens
+			if selected_token.get_tile() == tile:
+				tile.modulate = Color.white
+				selected_token = null
+			
+			# move if valid
+			elif is_valid_move(cur_player, selected_token, tile):
+				confirm_move(tile)
 		
 		# no token is selected; player is trying to select a token to play
 		else:
-			
-			if tile in get_end_tiles():
-				print("Cannot move tokens off of end tile")
-				return
-			
-			# get token on selected tile (if possible)
 			selected_token = get_player_token_on_tile(tile)
-			if selected_token:
-				if not selected_token in player_tokens[cur_player - 1]:
-					print("Not your token!")
-					selected_token = null
-					return
-				print("Token selected")
+			if selected_token \
+			and not tile in get_end_tiles() \
+			and selected_token in player_tokens[cur_player - 1]:
 				tile.modulate = Color.yellow
-			else:
-				print("No token on tile")
+
+
+# if possible, places current player's token on the board
+# this is where most of the game rules reside
+func is_valid_move(player, token, to_tile):
+	var result = false
+	var tile_distance = get_tile_distance(token.get_tile(), to_tile, player_tiles[player - 1])
+	if tile_distance == cur_roll:
+		
+		# what token is at the tile where the player is trying to place
+		# the currently selected token?
+		var other_token = get_player_token_on_tile(to_tile)
+		
+		# token can be placed on empty tile
+		if not other_token:
+			result = true
+		
+		# knock opponent off if not on a re-roll tile
+		elif other_token in player_tokens[opponent(player) - 1] \
+		and not to_tile in reroll_tiles:
+			result = true
 	
-	# illegal: tile not in current player's path
+	return result
+
+
+# called when a move has been successfully made by current player
+func confirm_move(tile_played):
+	var opp_token = get_player_token_on_tile(tile_played)
+	if (opp_token):
+		opp_token.set_tile(get_start_tiles()[opponent(cur_player) - 1])
+	
+	# move the selected token and reset tiles
+	reset_tile(selected_token.get_tile())
+	selected_token.set_tile(tile_played)
+	
+	# game over?
+	if tile_played in get_end_tiles():
+		if get_end_token_count(cur_player) == PLAYER_TOKEN_COUNT:
+			win(cur_player)
+			return
 	else:
-		print("Not your tile!")
+		add_pawn_child(tile_played)
+	
+	# deselect token, update UI
+	selected_token = null
+	update_counters()
+	
+	# roll again if tile was reroll
+	if tile_played in reroll_tiles:
+		roll_dice()
+	else:
+		switch_player()
 
 
 # return the sum of four "2-sided" dice
@@ -135,12 +176,18 @@ func roll_dice():
 		sum += roll
 		dice_nodes[i].texture = DICE_IMGS[roll]
 		dice_nodes[i].modulate = PLAYER_COLORS[cur_player - 1]
-	print("Player ", str(cur_player), " rolled ", sum)
 	cur_roll = sum
 	
 	# wait a bit before switching players so user sees the roll was 0
 	if cur_roll == 0:
+		cur_roll = -1
 		$ZeroRollTimer.start()
+	
+	# player's turn is skipped if they have no valid moves
+	elif not can_play(cur_player, cur_roll):
+		cur_roll = -1
+		set_message("No available moves")
+		$NoMovesTimer.start()
 
 
 # switch player and roll dice
@@ -149,22 +196,16 @@ func switch_player():
 		cur_player = 2
 	else:
 		cur_player = 1
-	#print("Player ", str(cur_player), "'s turn")
-	print()
+	update_player_message()
 	roll_dice()
 
 
 # return distance between two tiles
-func get_tile_distance(from, to):
-	var tile_list = player_tiles[cur_player - 1]
-	
-	var i1 = tile_list.find(from)
-	var i2 = tile_list.find(to)
-	
-	return i2 - i1
+func get_tile_distance(from, to, tile_list):
+	return tile_list.find(to) - tile_list.find(from)
 
 
-# returns the player token on the specified tile (if one exists)
+# returns the first player token on the specified tile (if one exists)
 func get_player_token_on_tile(tile):
 	var token = null
 	for token_list in player_tokens:
@@ -193,94 +234,9 @@ func reset_tile(tile):
 
 # game over!
 func win(player):
-	# TODO end game stuff
 	for btn in get_tree().get_nodes_in_group("tiles"):
 		btn.disabled = true
-	#Console.write_line("Hello world")
-	print("Congrats player ", player, ", you win!")
-
-
-# set button text,
-func end_turn(tile_played):
-	
-	# move the selected token and reset tiles
-	reset_tile(selected_token.get_tile())
-	selected_token.set_tile(tile_played)
-	
-	# did token make it to the end?
-	if tile_played in get_end_tiles():
-		
-		# end of game?
-		if get_end_tokens(cur_player) == PLAYER_TOKEN_COUNT:
-			win(cur_player)
-			return
-		
-	else:
-		add_pawn_child(tile_played)
-	
-	# deselect token
-	selected_token = null
-	
-	# roll again if tile was reroll
-	if tile_played in reroll_tiles:
-		roll_dice()
-	else:
-		switch_player()
-
-
-# if possible, places current player's token on the board
-# this is where most of the game rules reside
-func place_selected_token(tile):
-	var has_moved = false
-	
-	# what token is at the tile where the player is trying to place
-	# the currently selected token?
-	var other_token = get_player_token_on_tile(tile)
-	
-	var opponent = 1 - (cur_player - 1)
-	var opponent_tokens = player_tokens[opponent]
-	var opponent_start = get_end_tiles()[opponent]
-	
-	# make sure move matches roll
-	var tile_distance = get_tile_distance(selected_token.get_tile(), tile)
-	if tile_distance == cur_roll:
-		
-		# play token on empty tile
-		if not other_token:
-			print("Moving player piece")
-			has_moved = true
-		
-		# knock opponent off
-		elif other_token in opponent_tokens:
-			
-			# can only knock opponent off if not on reroll tile
-			if not tile in reroll_tiles:
-				print("Opponent piece captured!")
-				other_token.set_tile(opponent_start)
-				has_moved = true
-			else:
-				print("Cannot bump player off reroll tile")
-		
-		# illegal: one token allowed per tile
-		else:
-			print("One token allowed per tile")
-	
-	# move did not match roll
-	else:
-		
-		# deselect token by clicking it again
-		if tile_distance == 0:
-				selected_token = null
-				print("Token deselected")
-				tile.modulate = Color.white
-		
-		# illegal: wrong number of tiles
-		else:
-			print("Move did not match roll")
-	
-	if has_moved:
-		end_turn(tile)
-		update_counters()
+	set_message("Player %s wins!" % str(cur_player))
 
 
 # returns true if this tile has a default icon
@@ -315,14 +271,14 @@ func add_pawn_child(node):
 
 # updates the players' pawn counters
 func update_counters():
-	$rows/tiles/cols5/p1counter/text.text = str(get_start_tokens(1))
-	$rows/tiles/cols5/p2counter/text.text = str(get_start_tokens(2))
-	$rows/tiles/cols6/p1counter/text.text = str(get_end_tokens(1))
-	$rows/tiles/cols6/p2counter/text.text = str(get_end_tokens(2))
+	$rows/tiles/cols5/p1counter/text.text = str(get_start_token_count(1))
+	$rows/tiles/cols5/p2counter/text.text = str(get_start_token_count(2))
+	$rows/tiles/cols6/p1counter/text.text = str(get_end_token_count(1))
+	$rows/tiles/cols6/p2counter/text.text = str(get_end_token_count(2))
 
 
 # returns the specified player's number of tokens not yet played
-func get_start_tokens(player):
+func get_start_token_count(player):
 	var score = 0
 	for t in player_tokens[player - 1]:
 		if t.get_tile() == get_start_tiles()[player - 1]:
@@ -331,7 +287,7 @@ func get_start_tokens(player):
 
 
 # returns the specified player's number of tokens in the end tile
-func get_end_tokens(player):
+func get_end_token_count(player):
 	var score = 0
 	for t in player_tokens[player - 1]:
 		if t.get_tile() == get_end_tiles()[player - 1]:
@@ -347,3 +303,37 @@ func get_start_tiles():
 # returns end tiles of both players
 func get_end_tiles():
 	return [player_tiles[0][-1], player_tiles[1][-1]]
+
+
+# updates the message shown to player about game info
+func update_player_message():
+	set_message("%sst player's turn" % str(cur_player))
+
+
+# updates the UI text field with the specified message
+# and updates it's modulate color if provided, otherwise
+# modulate color is set to current player's color
+func set_message(message, color=null):
+	$rows/message.text = message
+	if not color:
+		color = PLAYER_COLORS[cur_player - 1]
+	$rows/message.modulate = color
+
+
+# returns true if any token has a valid move with the current roll,
+# otherwise returns false
+func can_play(player, roll):
+	var p = player - 1
+	for token in player_tokens[p]:
+		var p_tiles = player_tiles[p]
+		var to_index = p_tiles.find(token.get_tile()) + roll
+		if to_index < len(p_tiles):
+			var to_tile = p_tiles[to_index]
+			if is_valid_move(player, token, to_tile):
+				return true
+	return false
+
+
+# returns the opponent of the specified player
+func opponent(player):
+	return 1 - (player - 1)
